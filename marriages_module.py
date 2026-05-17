e681f3bba605ad4ccfa0dbe8d9283db4ac9f3a5a
jellymainy <127957016+jellymainy@users.noreply.github.com>
2026-05-18 01:14:28 +0300

    Обработка статистики по бракам
    Файл для обработки данных о браках и разводах, добавлен метод для вывода статистики

---
+++ marriages_module.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from services.forecast_service import moving_average_forecast
+
+class MarriagesModule:
+
+    def __init__(self):
+        self.data = None
+
+    def load_data(self, path):
+        self.data = pd.read_csv(path)
+        
+        print("\nТАБЛИЦА")
+        print(self.data.to_string(index=False))
+
+    def show_statistics(self):
+        print("\nСТАТИСТИКА")
+        
+        male_marriage = self.data["male_peak_marriage_age"].mode()[0]
+        female_marriage = self.data["female_peak_marriage_age"].mode()[0]
+        male_divorce = self.data["male_peak_divorce_age"].mode()[0]
+        female_divorce = self.data["female_peak_divorce_age"].mode()[0]
+        
+        print("Мужчины чаще женились:", male_marriage)
+        print("Женщины чаще выходили замуж:", female_marriage)
+        print("Мужчины чаще разводились:", male_divorce)
+        print("Женщины чаще разводились:", female_divorce)
